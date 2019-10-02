Snow depth temperature correct. Updates to data parsing. Updates to flux calculation.

DataParse/eddy_flux.py
@@ -244,9 +244,14 @@ def shf(w,t,avp):
     for i in range(0,len(keys)):
         w1 =w_g.get_group(keys[i])
         t1 =t_g.get_group(keys[i])
-        flux,std = eddyflux(w1,t1) # Flux units = Km/s
-        SHF.append(rho * cp * flux) # W/m2
-        stds.append(std)
+        # Check there's at least 60% of averaging period data
+        if len(t1) >= 0.6 * (15*60) * 10:
+            flux,std = eddyflux(w1,t1) # Flux units = Km/s
+            SHF.append(rho * cp * flux) # W/m2
+            stds.append(std)
+        else:
+            SHF.append(np.nan)
+            stds.append(np.nan)
     
     return SHF,std
 
@@ -273,13 +278,17 @@ def lhf(w,T,P,Nconc,avp):
         # Join data frames so they're the same length, remove nans
         join = pd.concat([q, w1], axis=1, sort=False)
         join = join.dropna()
-
-        # Calculate latent heat flux.
-        #It represents a loss of energy from the
-        #surface due to evaporation. 
-        # LH = L * rho * mean(w'*q')
-        # q = H2O mixing ratio
-        flux,std = eddyflux(join['w'],join[0])
-        LHF.append(L * rho * flux)
+        
+        # Check there's at least 60% of averaging period data
+        if len(join) >= 0.6 * (15*60) * 10:
+            # Calculate latent heat flux.
+            #It represents a loss of energy from the
+            #surface due to evaporation. 
+            # LH = L * rho * mean(w'*q')
+            # q = H2O mixing ratio
+            flux,std = eddyflux(join['w'],join[0])
+            LHF.append(L * rho * flux)
+        else:
+            LHF.append(np.nan)
         
     return LHF

DataParse/fluxtower_parse.py
@@ -99,7 +99,7 @@ def extract_snd_data(start,stop,dpath,log_snd):
 
     # Extract daterange
     file_dates = np.asarray([int(f[0:6]) for f in all_files])
-    idxs = np.where(np.logical_and(file_dates>=start_f, file_dates<=stop_f))[0]
+    idxs = np.where(np.logical_and(file_dates>=start_f, file_dates<stop_f))[0]
     dfs = [all_files[i] for i in idxs]
 
     # Initialise empty data frames
@@ -134,10 +134,22 @@ def extract_snd_data(start,stop,dpath,log_snd):
     if snd.empty==False:
         snd = snd.set_index('Date')
         snd = snd.sort_values('Date')
-        new_idx = pd.date_range(pd.to_datetime(str(start_f),format='%y%m%d'),pd.to_datetime(str(stop_f),format='%y%m%d'),freq='min' )
+    #    new_idx = pd.date_range(pd.to_datetime(str(start_f),format='%y%m%d'),pd.to_datetime(str(stop_f),format='%y%m%d'),freq='min' )
         snd.index = pd.DatetimeIndex(snd.index)
         snd = snd[~snd.index.duplicated()]
-        snd= snd.reindex(new_idx, fill_value=np.NaN)    
+    #   snd= snd.reindex(new_idx, fill_value=np.NaN)
+        
+        # Check diagnostic tests pass.
+        snd = snd[snd['V'].astype(int)==11]
+
+        # Check data quality
+        snd['Q'] = snd['Q'].astype(int)
+        snd = snd[snd['Q']>151]
+
+        # Check for crazy values
+        snd['diff']=snd['depth'].diff()
+        snd = snd[np.abs(snd['diff'])<0.1] # Ignore if 10 minute change greater than 10cm in 10 minutes
+        
     else: 
         log.write('No data from snd\n')
        
@@ -168,15 +180,15 @@ def HMP_pdf_sort(df,start,stop):
         df['Ta']=df['Ta'].astype(float)
         #df['h']=df['h'].astype(int)       
         df = df.sort_values('Date')
-        new_idx = pd.date_range(pd.to_datetime(start).round('1s'),pd.to_datetime(stop).round('1s'),freq='1s' )
+        #new_idx = pd.date_range(pd.to_datetime(start).round('1s'),pd.to_datetime(stop).round('1s'),freq='1s' )
         df.index = pd.DatetimeIndex(df.index)
         df = df[~df.index.duplicated()]
-        #df= df.reindex(new_idx, fill_value=np.NaN)
+        #df= df.reindex(new_idx, method='nearest')
     else:
         df = pd.DataFrame(columns=['RH', 'Ta', 'Tw', 'Err', 'h'])
     return df
 
-def extract_HMP_data(name, start,stop,dpath,logf):
+def extract_HMP_data(name, start,stop,dpath,logf, save):
     # Extract HMP155 data into a pandas array
     # Data format: YYYY MM DD HH MM.mmm TT:TT:TT RH Ta Tw Err hs
     # Ta = seperate probe T, Tw = wetbulb t, hs = heating status
@@ -218,6 +230,10 @@ def extract_HMP_data(name, start,stop,dpath,logf):
     HMP = HMP_pdf_sort(HMP,start,stop)
 
     log.write('Data parse finished\n')
+    if save: 
+        HMP.to_csv(save+'%s_%s'%(name,str(start.date())))
+        log.write('Saved csv')
+
     log.close()
     return HMP
 